a6e570a573f8a8fd6590c99814fe8dc95a0dff94
happou31 <happo31@gmail.com>
2017-11-07 14:24:47 +0900

    Add Block manager scripting.

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -2,15 +2,78 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BlockScript : MonoBehaviour {
+public class Block : MonoBehaviour {
+
+    private KIND kind;
+
+    public KIND Kind
+    {
+        get
+        {
+            return kind;
+        }
+
+        set
+        {
+            kind = value;
+        }
+    }
+
+    public Sprite NoneSprite;
+    public Sprite SpringSprite;
+    public Sprite SummerSprite;
+    public Sprite FallSprite;
+    public Sprite WinterSprite;
+
+    SpriteRenderer spriteRenderer;
+    
+    Block(KIND kind = KIND.NONE)
+    {
+        this.kind = kind;
+    }
 
 	// Use this for initialization
 	void Start () {
-		
-	}
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = GetKindSprite(this.kind);
+    }
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+
+    private Sprite GetKindSprite(KIND kind)
+    {
+        switch (kind)
+        {
+            case KIND.NONE:
+                return NoneSprite;
+            case KIND.SPRING:
+                return SpringSprite;
+            case KIND.SUMMER:
+                return SummerSprite;
+            case KIND.FALL:
+                return FallSprite;
+            case KIND.WINTER:
+                return WinterSprite;
+            default:
+                return NoneSprite;
+        }
+    }
+
+    public static KIND GetRandomBlockKind(bool generateNone = false)
+    {
+        return !generateNone ? (KIND)Random.Range(1, 4) : (KIND)Random.Range(0, 4);
+    }
+
+    public enum KIND
+    {
+        NONE,
+        SPRING,
+        SUMMER,
+        FALL,
+        WINTER
+    }
 }
---
+++ Assets/Scripts/BlockManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockManager : MonoBehaviour {
+
+    public Block blockPrefab;
+
+    private List<Block> blocks = new List<Block>();
+
+
+    private const float BASEY = 4.0f;
+    private const float BASEX = 7.0f;
+    private const float BASELENGTH = 2.6f;
+
+    public float FieldLength;
+
+	// Use this for initialization
+	void Start () {
+
+        for (var i = 0; i < FieldLength * FieldLength; ++i)
+        {
+            var obj = Instantiate<Block>(blockPrefab);
+            float x = i % FieldLength * BASELENGTH * BASEX;
+            float y = i / FieldLength * BASELENGTH * BASEY;
+            Debug.Log("x:" + x.ToString() + " y:" + y.ToString());
+            obj.transform.position = new Vector3(x, y);
+            obj.Kind = Block.GetRandomBlockKind();
+
+            blocks.Add(obj);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
