f3138720f0e3c5265458a9ef96c44306a1942d52
happou31 <happo31@gmail.com>
2017-11-18 23:27:10 +0900

    Add method.

--- Assets/Scripts/Util/ListPositionMappingExtendMethod.cs
+++ Assets/Scripts/Util/ListPositionMappingExtendMethod.cs
@@ -10,4 +10,24 @@ public static class ListPositionMappingExtendMethod {
     {
         list[y * width + x] = value;
     }
+
+    public static bool InField<T>(this List<T> list, int x, int y, int width)
+    {
+        return x >= 0 && y >= 0 && x < width && y < width;
+    }
+
+    public static T PositionAt<T>(this T[] list, int x, int y, int width)
+    {
+        return list[y * width + x];
+    }
+
+    public static void SetValuePosition<T>(this T[] list, int x, int y, int width, T value)
+    {
+        list[y * width + x] = value;
+    }
+
+    public static bool InField<T>(this T[] list, int x, int y, int width)
+    {
+        return x >= 0 && y >= 0 && x < width && y < width;
+    }
 }
